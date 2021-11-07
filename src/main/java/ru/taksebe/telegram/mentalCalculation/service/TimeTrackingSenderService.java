df29d9b96814b3716f76ff06e7b9b1ed6277b862
Ilia <rybalko94ia@gmail.com>
2021-11-07 21:32:21 +0300

    SendMessage class and pom

---
+++ src/main/java/ru/taksebe/telegram/mentalCalculation/service/TimeTrackingSenderService.java
@@ -0,0 +1,24 @@
+package ru.taksebe.telegram.mentalCalculation.service;
+
+
+import ru.taksebe.telegram.mentalCalculation.telegram.Bot;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
+public class TimeTrackingSenderService {
+    Bot bot;
+
+    public TimeTrackingSenderService() {
+    }
+
+    @WebMethod
+    public void sendTacking(Long chatId, String userName, String text) {
+
+    }
+
+    public void setBot(Bot bot) {
+        this.bot = bot;
+    }
+}
--- src/main/java/ru/taksebe/telegram/mentalCalculation/telegram/commands/operations/CommandCommand.java
+++ src/main/java/ru/taksebe/telegram/mentalCalculation/telegram/commands/operations/CommandCommand.java
@@ -7,7 +7,7 @@ import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 
 public class CommandCommand extends OperationCommand {
-    private Logger logger = LoggerFactory.getLogger(CommandCommand.class);
+    private final Logger logger = LoggerFactory.getLogger(CommandCommand.class);
 
     public CommandCommand(String identifier, String description) {
         super(identifier, description);
@@ -15,17 +15,17 @@ public class CommandCommand extends OperationCommand {
 
     @Override
     public void execute(AbsSender absSender, User user, Chat chat, String[] strings) {
-        TestInterface testInterface = new TestInterfaceImplService()
-                .getPort(TestInterface.class);
-
-        logger.info("Invocation of SOAP Server - start.");
+//        TestInterface testInterface = new TestInterfaceImplService()
+//                .getPort(TestInterface.class);
+//
+//        logger.info("Invocation of SOAP Server - start.");
 
 //        System.out.println("/create called");
 
-        String res = testInterface.hello();
-
-        logger.info("Invocation of SOAP Server - end. Employee's last name: "
-                + res);
+//        String res = testInterface.hello();
+//
+//        logger.info("Invocation of SOAP Server - end. Employee's last name: "
+//                + res);
 //        System.out.println(employee.getLastName());
     }
 }
