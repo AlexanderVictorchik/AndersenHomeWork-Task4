dec8a2ac2520db142d7efa03ad798665b5569dc5
Ilia <rybalko94ia@gmail.com>
2021-11-07 17:50:23 +0300

    naming and bot injection

--- src/main/java/ru/taksebe/telegram/mentalCalculation/MentalCalculationApplication.java
+++ src/main/java/ru/taksebe/telegram/mentalCalculation/MentalCalculationApplication.java
@@ -3,6 +3,7 @@ package ru.taksebe.telegram.mentalCalculation;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import ru.taksebe.telegram.mentalCalculation.soapservices.TimeTrackingSenderService;
 import ru.taksebe.telegram.mentalCalculation.telegram.Bot;
 
 import java.util.Map;
@@ -13,7 +14,11 @@ public class MentalCalculationApplication {
     public static void main(String[] args) {
         try {
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            botsApi.registerBot(new Bot("andersentimetrackingbot", "2128890774:AAEzTh0oxx7JlUmyJeRhyzkHhgfNKx1R4OM"));
+            Bot bot = new Bot("andersentimetrackingbot", "2128890774:AAEzTh0oxx7JlUmyJeRhyzkHhgfNKx1R4OM");
+            botsApi.registerBot(bot);
+            TimeTrackingSenderService timeTrackingSenderService = new TimeTrackingSenderService();
+            timeTrackingSenderService.setBot(bot);
+
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
